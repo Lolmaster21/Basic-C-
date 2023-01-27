a356a50ad1e7a5dbcbd9e932f7713011cbaa665a
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-01-27 09:41:00 -0700

    Create switch statement.cpp

---
+++ switch statement.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  char character;
+  cout << "Again what is your favorite character?" << endl;
+	cin >> character;
+
+	void cartoon(char character);
+		switch (character) {
+
+		case 'b':
+			cout << ("Eat my shorts") << endl;
+			break;
+
+		case 'd':
+
+			cout << ("sufferin' succotash!") << endl;
+
+
+		case 'r':
+			cout << ("Wubba lubba dub dub") << endl;
+
+			break;
+
+		case 's':
+			cout << ("scooby dooby doo") << endl;
+				
+			break;
+
+
+
+		}
+}
