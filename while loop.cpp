433f0a8c80fcf1be7b9c318003a06fe3cec82478
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-02-07 08:50:48 -0700

    Create while loop.cpp

---
+++ while loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	char choice = 'a';
+	while (choice != 'q') {
+
+		cout << "press any key, q to quit" << endl;
+		cin >> choice;
+		cout << "You typed " << choice << endl;
+
+	}
+
+
+
+
+}
